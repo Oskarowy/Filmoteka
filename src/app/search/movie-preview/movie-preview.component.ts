50fa2e8a50fbdf2801befc175b8ba60639019d0a
Oskarovski <oskar.bialek.programista@gmail.com>
2018-10-22 14:44:01 +0200

    Binded data from mock file to get properties of Movie class

--- src/app/search/movie-preview/movie-preview.component.ts
+++ src/app/search/movie-preview/movie-preview.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../../Movie';
 @Component({
   selector: 'movie-preview',
   templateUrl: './movie-preview.component.html',
   styles: []
 })
 export class MoviePreviewComponent implements OnInit {
-
+  @Input() movie:Movie = {}
   constructor() { }
 
   ngOnInit() {
